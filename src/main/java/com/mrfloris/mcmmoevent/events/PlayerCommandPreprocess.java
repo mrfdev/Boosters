f955dd5f6210f1ec1b62b6e03f8e97ecfb42cdaf
The456gamer <The456gamer@the456gamer.dev>
2021-05-19 20:27:15 +0100

    re-write both command handlers

--- src/main/java/com/mrfloris/mcmmoevent/events/PlayerCommandPreprocess.java
+++ src/main/java/com/mrfloris/mcmmoevent/events/PlayerCommandPreprocess.java
@@ -1,63 +1,47 @@
 package com.mrfloris.mcmmoevent.events;
 
-import org.bukkit.entity.Player;
+import com.google.common.primitives.Ints;
+import com.mrfloris.mcmmoevent.mcMMOEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-
-import com.mrfloris.mcmmoevent.mcMMOEvent;
 
 public class PlayerCommandPreprocess implements Listener {
 
     private final mcMMOEvent plugin;
 
-    int tryparse(String tryset) {
-        try {
-            return Integer.parseInt(tryset);
-        }
-        catch (NumberFormatException e) {
-            // yes
-        }
-        return Integer.parseInt(tryset);
-    }
     public PlayerCommandPreprocess(mcMMOEvent plugin) {
         this.plugin = plugin;
     }
     @EventHandler
     public void on(PlayerCommandPreprocessEvent e) {
-        String cmd = e.getMessage();
-        Player p = e.getPlayer();
-        // TODO: check if command is xprate, then check for permission, no need to check permission on every command <zrips>
-//        @EventHandler
-//        public void on(PlayerCommandPreprocessEvent e) {
-//            String cmd = e.getMessage();
-//
-//            if (!cmd.toLowerCase().startsWith("/xprate "))
-//                return;
-//
-//            Player p = e.getPlayer();
-//            if (!p.hasPermission("mcmmo.commands.xprate.all") &&
-//                    !p.hasPermission("mcmmo.commands.xprate") &&
-//                    !p.hasPermission("mcmmo.commands.xprate.reset") &&
-//                    !p.hasPermission("mcmmo.commands.xprate.set") &&
-//                    !p.isOp())
-//                return;
-//
-//            if (cmd.equalsIgnoreCase("/xprate true") || cmd.equalsIgnoreCase("/xprate false")) {
-//                plugin.setRate(Integer.parseInt(cmd.split(" ")[1]));
-//            } else if (cmd.contains("/xprate ") && cmd.equalsIgnoreCase("/xprate reset") || cmd.equalsIgnoreCase("/xprate clear")) {
-//                plugin.setRate(1);
-//            }
-//        }
-        if (p.hasPermission("mcmmo.commands.xprate.all") || p.hasPermission("mcmmo.commands.xprate")
-                || p.hasPermission("mcmmo.commands.xprate.reset") || p.hasPermission("mcmmo.commands.xprate.set")) {
-            if (cmd.contains("/xprate ") && (cmd.contains(" true") || cmd.contains(" false"))) {
-                plugin.setRate(tryparse(cmd.split(" ")[1]));
-            } else {
-                if (cmd.contains("/xprate ") && cmd.equalsIgnoreCase("/xprate reset") || cmd.equalsIgnoreCase("/xprate clear")) {
+        // mcmmo.commands.xprate.all
+        // mcmmo.commands.xprate // probably only this one is checked for.
+        // mcmmo.commands.xprate.reset
+        // mcmmo.commands.xprate.set
+
+        String input = e.getMessage();
+        if (input.startsWith("/")){
+            input = input.substring(1);
+            plugin.getLogger().info("DEBUG:before substring = \""+e.getMessage()+"\"; after = \""+input+"\"");
+        }
+        String[] cmd = input.split(" ");
+        if (!cmd[0].equalsIgnoreCase("xprate")) { return; }
+        if (!e.getPlayer().hasPermission("mcmmo.commands.xprate")) { return; } // not the best way, but this whole thing is hacky.
+        switch (cmd.length) {
+            case 2:
+                if (cmd[1].equalsIgnoreCase("reset") || cmd[1].equalsIgnoreCase("clear")) {
                     plugin.setRate(1);
                 }
-            }
+            case 3:
+                if (!cmd[2].equalsIgnoreCase("true") && !cmd[2].equalsIgnoreCase("false")) {
+                    return;
+                }
+                Integer rate = Ints.tryParse(cmd[1]); // this should always exist
+                if  (rate == null) { return; }
+                plugin.setRate(rate);
+                return;
+            default: // triggers on no args (length 1), and too many (length 4+)
         }
     }
 }
--- src/main/java/com/mrfloris/mcmmoevent/events/ServerCommand.java
+++ src/main/java/com/mrfloris/mcmmoevent/events/ServerCommand.java
@@ -1,13 +1,10 @@
 package com.mrfloris.mcmmoevent.events;
 
 import com.google.common.primitives.Ints;
+import com.mrfloris.mcmmoevent.mcMMOEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerCommandEvent;
-
-import com.mrfloris.mcmmoevent.mcMMOEvent;
-
-import java.util.List;
 
 public class ServerCommand implements Listener {
 
@@ -20,33 +17,31 @@ public class ServerCommand implements Listener {
     @EventHandler
     public void on(ServerCommandEvent e) {
         /*
-            /xprate <rate:int> [broadcast:bool]
+            cmd length 1 - invalid
+            cmd length 2
+            /xprate reset - valid
+            /xprate clear - valid
+            /xprate anything else - invalid
+            cmd length 3
+            /xprate valid-int valid-bool - valid
+
          */
         String[] cmd = e.getCommand().split(" ");
-        if (!cmd[0].equalsIgnoreCase("xprate")) {
-            return;
-        }
-
+        if (!cmd[0].equalsIgnoreCase("xprate")) { return; }
         switch (cmd.length) {
             case 2:
-                break; // break moves on to next section
-            case 3:
-                if (cmd[2].equalsIgnoreCase("true") || cmd[2].equalsIgnoreCase("false")) {
-                    break;
+                if (cmd[1].equalsIgnoreCase("reset") || cmd[1].equalsIgnoreCase("clear")) {
+                    plugin.setRate(1);
                 }
-                return; // invalid command
-
-            default: // triggers on no args (length 1), and too many (length 4+)
+            case 3:
+                if (!cmd[2].equalsIgnoreCase("true") && !cmd[2].equalsIgnoreCase("false")) {
+                    return;
+                }
+                Integer rate = Ints.tryParse(cmd[1]); // this should always exist
+                if  (rate == null) { return; }
+                plugin.setRate(rate);
                 return;
+            default: // triggers on no args (length 1), and too many (length 4+)
         }
-        if (cmd[1].equalsIgnoreCase("reset") || cmd[1].equalsIgnoreCase("clear")) {
-            plugin.setRate(1);
-            return;
-        }
-        Integer rate = Ints.tryParse(cmd[1]); // this should always exist
-        if  (rate == null) {
-            return;
-        }
-        plugin.setRate(rate);
     }
 }
