ed6fab74ee2d7ecfc93fd38c7fcdd910e2b23d79
The456gamer <The456gamer@the456gamer.dev>
2021-05-19 01:26:47 +0100

    Impliment proper argument parsing for the console command

--- src/main/java/com/mrfloris/mcmmoevent/events/ServerCommand.java
+++ src/main/java/com/mrfloris/mcmmoevent/events/ServerCommand.java
@@ -1,10 +1,13 @@
 package com.mrfloris.mcmmoevent.events;
 
+import com.google.common.primitives.Ints;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerCommandEvent;
 
 import com.mrfloris.mcmmoevent.mcMMOEvent;
+
+import java.util.List;
 
 public class ServerCommand implements Listener {
 
@@ -16,13 +19,34 @@ public class ServerCommand implements Listener {
 
     @EventHandler
     public void on(ServerCommandEvent e) {
-        String cmd = e.getCommand();
-        if (cmd.contains("xprate ") && (cmd.contains(" true") || cmd.contains(" false"))) {
-            plugin.setRate(Integer.parseInt(cmd.split(" ")[1]));
-        } else {
-            if (cmd.contains("xprate ") && cmd.equalsIgnoreCase("xprate reset") || cmd.equalsIgnoreCase("xprate clear")) {
-                plugin.setRate(1);
-            }
+        /*
+            /xprate <rate:int> [broadcast:bool]
+         */
+        String[] cmd = e.getCommand().split(" ");
+        if (!cmd[0].equalsIgnoreCase("xprate")) {
+            return;
         }
+
+        switch (cmd.length) {
+            case 2:
+                break; // break moves on to next section
+            case 3:
+                if (cmd[2].equalsIgnoreCase("true") || cmd[2].equalsIgnoreCase("false")) {
+                    break;
+                }
+                return; // invalid command
+
+            default: // triggers on no args (length 1), and too many (length 4+)
+                return;
+        }
+        if (cmd[1].equalsIgnoreCase("reset") || cmd[1].equalsIgnoreCase("clear")) {
+            plugin.setRate(1);
+            return;
+        }
+        Integer rate = Ints.tryParse(cmd[1]); // this should always exist
+        if  (rate == null) {
+            return;
+        }
+        plugin.setRate(rate);
     }
 }
